23910081c7b0e98eab73d93b73b5490a1219c7be
zakibn18 <jekben815@gmail.com>
2026-03-12 22:41:32 +0700

    Menambahkan query SELECT COUNT(*) untuk mahasiswa pada tombol btnHitungMhs

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -47,7 +47,20 @@ namespace PraktikumADO
 
         private void btnHitungMhs_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                Koneksi();
+                conn.Open();
+                string query = "SELECT COUNT(*) FROM Mahasiswa";
+                cmd = new SqlCommand(query, conn);
+                int jumlah = (int)cmd.ExecuteScalar();
+                txtHasil.Text = jumlah.ToString();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
